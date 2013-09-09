71af82a011ef3e6da6b475f66cb816ac7e1b36a6
Jason <jfingar@gmail.com>
2013-09-08 19:59:06 -0700

    added attachment method to Mail class

--- application/libraries/TinyPHP/Mail.php
+++ application/libraries/TinyPHP/Mail.php
@@ -3,6 +3,7 @@ namespace Libraries\TinyPHP;
 use \Swift_Message;
 use \Swift_SmtpTransport;
 use \Swift_Mailer;
+use \Swift_Attachment;
 class Mail
 {
     private $recipients = array();
@@ -10,6 +11,7 @@ class Mail
     private $type = 'text/html';
     private $subject;
     private $body;
+    private $attachments = array();
     private static $_smtpHost;
     private static $_smtpPort;
     private static $_smtpUsername;
@@ -40,17 +42,26 @@ class Mail
         $this->body = $body;
     }
     
-    public function addAttachment($attachment)
+    public function addAttachment($attachmentPath)
     {
-        //TODO: implement this
+        $this->attachments[] = $attachmentPath;
     }
     
     public function send()
     {
         $message = Swift_Message::newInstance($this->subject);
         $message->setFrom($this->from);
+        if(empty($this->recipients)){
+            throw new Exception("Please add at least 1 recipient!");
+            return;
+        }
         foreach($this->recipients as $recipientEmail){
             $message->addTo($recipientEmail);
+        }
+        if(!empty($this->attachments)){
+            foreach($this->attachments as $attachmentPath){
+                $message->attach(Swift_Attachment::fromPath($attachmentPath));
+            }
         }
         $message->setBody($this->body,$this->type);
         
