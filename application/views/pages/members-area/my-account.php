21d30016a9d3ce16a0d523b5a34de083b1ce55e8
Jason <jfingar@gmail.com>
2013-09-22 19:56:28 -0700

    css

--- application/views/pages/members-area/my-account.php
+++ application/views/pages/members-area/my-account.php
@@ -4,3 +4,21 @@
     </h1>
     <div class="clear"></div>
 </div>
+
+<form id="member-data">
+    <div class="member-data-input-container">
+        <label>First Name</label>
+        <input type="text" class="text-input-1" name="firstName" />
+    </div>
+    <div class="member-data-input-container">
+        <label>Last Name</label>
+        <input type="text" class="text-input-1" name="lastName" />
+    </div>
+    <div class="member-data-input-container">
+        <label>Email Address</label>
+        <input type="text" class="text-input-1" name="email" />
+    </div>
+    <div class="clear"></div>
+</form>
+
+<a id="show-update-pw">CHANGE PASSWORD</a>
