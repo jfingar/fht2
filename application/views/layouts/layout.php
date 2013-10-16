77288def35c940dfb838170ee9b2af95a34e4c4a
Jason <jfingar@gmail.com>
2013-10-16 15:17:02 -0700

    added new social widget

--- application/views/layouts/layout.php
+++ application/views/layouts/layout.php
@@ -5,6 +5,9 @@
 	<title><?= $this->title; ?></title>
 	<?= $this->getJavascripts(); ?>
 	<?= $this->getStylesheets(); ?>
+        <script type="text/javascript">var switchTo5x=true;</script>
+        <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+        <script type="text/javascript" src="http://s.sharethis.com/loader.js"></script>
 </head>
 <body>
     <div id="notification"></div>
@@ -23,5 +26,10 @@
     <div id="footer">
         Please <a href="mailto:support@freehandicaptracker.net">contact the website owner</a> with any questions or comments regarding usage of this site. Thank you for visiting!
     </div>
+    <script type="text/javascript">stLight.options({publisher: "cecdced8-07c2-4546-8e59-20306ef31079", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+    <script>
+    var options={ "publisher": "cecdced8-07c2-4546-8e59-20306ef31079", "position": "left", "ad": { "visible": false, "openDelay": 5, "closeDelay": 0}, "chicklets": { "items": ["facebook", "twitter", "googleplus"]}};
+    var st_hover_widget = new sharethis.widgets.hoverbuttons(options);
+    </script>
 </body>
 </html>
--- application/views/pages/register/index.php
+++ application/views/pages/register/index.php
@@ -82,3 +82,6 @@
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
 <div class="clear"></div>
+<div id="tandc-modal">
+    
+</div>
