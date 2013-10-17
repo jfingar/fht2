a6803e9a82e1230489c189f78fbbf75b8dadbce6
Jason <jfingar@gmail.com>
2013-10-17 13:57:08 -0700

    add google analytics code

--- application/views/layouts/layout.php
+++ application/views/layouts/layout.php
@@ -9,6 +9,17 @@
         <script type="text/javascript">var switchTo5x=true;</script>
         <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
         <script type="text/javascript" src="http://s.sharethis.com/loader.js"></script>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-16929955-2']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+      </script>
 </head>
 <body>
     <div id="notification"></div>
