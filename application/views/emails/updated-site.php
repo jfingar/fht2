1a41bb96ea1c2395333e0e3042f6282c21719614
Jason <jfingar@gmail.com>
2013-10-16 21:40:33 -0700

    remove beta. from url

--- application/views/emails/updated-site.php
+++ application/views/emails/updated-site.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <div style="width: 600px;font-family: 'Arial','Helvetica',sans-serif">
-            <img src="http://beta.freehandicaptracker.net/img/e-mail-header.jpg" alt="FreeHandicapTracker.net" />
+            <img src="http://freehandicaptracker.net/img/e-mail-header.jpg" alt="FreeHandicapTracker.net" />
             <div style="margin: 10px 0px 10px 0px;">Dear <?= $this->user->getFirstName(); ?>,</div>
             <div>
                 The new <a href="http://www.freehandicaptracker.net">FreeHandicapTracker</a> site was just launched today. The site is still free, and offers a lot of new features and improvements.
@@ -16,7 +16,7 @@
                 <li>New stats track number of rounds entered, average score, and best score</li>
                 <li>Easily view which rounds are used in your handicap index</li>
             </ul>
-            <img src="http://beta.freehandicaptracker.net/img/freehandicaptracker.jpg" alt="Preview of new FreeHandicapTracker.net website" />
+            <img src="http://freehandicaptracker.net/img/freehandicaptracker.jpg" alt="Preview of new FreeHandicapTracker.net website" />
             <div style="margin: 10px 0px 10px 0px;">Check out the new and improved site, and feel free to contact the webmaster at support@freehandicaptracker.net if you have any feedback.</div>
             <div>Thank you for your continued use of the site, and don't forget to spread the word!</div>
             <div style="margin: 10px 0px 10px 0px;">Sincerely,</div>
