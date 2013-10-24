65cbe7d0211eaa5c1e59be068270594b2e5e5d2a
Jason-Laptop <jfingar@gmail.com>
2013-10-24 16:58:43 -0700

    update to new cli interface

--- application/cli/emailer.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Libraries\TinyPHP\Application;
-use Controllers\CliController;
-chdir("../../webroot");
-require_once "../application/libraries/TinyPHP/Application.php";
-Application::run('cli',true);
-
-new CliController('PwResetMessupEmail');
--- application/controllers/CliController.php
+++ application/controllers/CliController.php
@@ -1,39 +1,10 @@
 <?php
 namespace Controllers;
 use Libraries\TinyPHP\ControllerBase;
-use Models\Mappers\PasswordReset;
-use Models\Mappers\User;
-use Libraries\TinyPHP\Mail;
-use \Exception;
 class CliController extends ControllerBase
 {
-    public function init()
+    public function SampleCliFunction()
     {
-        $this->suppressLayout = true;
-        $this->suppressView = true;
-    }
-    public function PwResetMessupEmail()
-    {
-        $pwResetMapper = new PasswordReset();
-        $rows = $pwResetMapper->fetchAll();
-        $userMapper = new User();
-        foreach($rows as $row){
-            $userEmailAddress = $row->getEmail();
-            $this->user = $userMapper->fetchRow("email = ?",array($userEmailAddress));
-            $emailContent = $this->returnView('emails/pw-reset-messup');
-            $emailSubject = "FreeHandicapTracker.net password reset fixed";
-            $mail = new Mail();
-            $mail->setSubject($emailSubject);
-            $mail->setFrom("support@freehandicaptracker.net","FreeHandicapTracker");
-            $mail->addRecipient($userEmailAddress);
-            $mail->setBody($emailContent);
-            try{
-                $mail->send();
-                echo "mail sent to: " . $userEmailAddress . "\r\n";
-            }catch(Exception $e){
-                echo $e->getMessage();
-            }
-            sleep(5);
-        }
+        
     }
 }
