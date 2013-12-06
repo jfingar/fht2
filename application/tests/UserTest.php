e8edc7038681f34dbc0ab5c298aac74562095b60
Jason <jfingar@gmail.com>
2013-12-05 20:19:08 -0700

    implemented unit test for registration

--- application/tests/UserTest.php
+++ application/tests/UserTest.php
@@ -4,16 +4,16 @@ use PHPUnit_Framework_TestCase;
 use Models\Helpers\User AS User_Helper;
 use Models\User AS User_Model;
 use Models\Mappers\User AS User_Mapper;
-class UserTest extends PHPUnit_Framework_TestCase
+use Libraries\TinyPHP\Db\Adapter;
+class RegisterTest extends PHPUnit_Framework_TestCase
 {
-    public function testRegister()
+    /**
+     * 
+     * @test
+     * @dataProvider provider
+     */
+    public function Register($firstName,$lastName,$email,$password1,$password2)
     {
-        $firstName = 'Joe';
-        $lastName = 'Schmoe';
-        $email = 'jschmoee34h533@yahoo.com';
-        $password1 = 'yolo';
-        $password2 = 'yolo';
-        
         $user = new User_Model();
         $user->setFirstName($firstName);
         $user->setLastName($lastName);
@@ -31,11 +31,39 @@ class UserTest extends PHPUnit_Framework_TestCase
         return $loginCredentials;
     }
     
-    /**
-     * @depends testRegister
-     */
-    public function testLogin(array $loginCredentials)
+    public function provider()
     {
-        $this->assertInstanceOf('Models\User',User_Helper::authenticate($loginCredentials['email'],$loginCredentials['password']));
+        $numberOfTestRegistrants = 10;
+        
+        $pdo = Adapter::GetMysqlAdapter();
+        
+        $firstNameQuery = 'SELECT firstname FROM test_data.first ORDER BY RAND() LIMIT 1';
+        $lastNameQuery = 'SELECT lastname FROM test_data.last ORDER BY RAND() LIMIT 1';
+        $data = array();
+        for($i = 1; $i <= $numberOfTestRegistrants; $i++){
+            $statement = $pdo->query($firstNameQuery);
+            $result = $statement->fetch();
+            $firstName = $result['firstname'];
+            
+            $statement = $pdo->query($lastNameQuery);
+            $result = $statement->fetch();
+            $lastName = $result['lastname'];
+            
+            $emailDomains = array('gmail','aol','hotmail','yahoo');
+            $email = substr($firstName,0,2) . $lastName . substr(time(),4,7) . '@' . $emailDomains[rand(0,3)] . '.com';
+            
+            $password1 = 'yolo';
+            $password2 = 'yolo';
+            
+            
+            $data[] = array(
+                $firstName,
+                $lastName,
+                $email,
+                $password1,
+                $password2
+            );
+        }
+        return $data;
     }
 }
