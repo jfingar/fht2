57ea93723d2a8b14201d37b3f7d0d881c3c366cc
Jason <jfingar@gmail.com>
2013-12-08 13:39:25 -0700

    back to square one on unit test

--- application/tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Libraries\TinyPHP\Application;
-use Controllers\CliController;
-require_once "../libraries/TinyPHP/Application.php";
-chdir("../../webroot");
-Application::run('development',true);
